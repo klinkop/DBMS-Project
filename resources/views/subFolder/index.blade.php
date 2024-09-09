bf773cef45bbe00a81206ee0ce3c5687227db417
Asrplynd <87851596+Asrplynd@users.noreply.github.com>
2024-09-09 10:35:24 +0800

    Add Header for Sub Folder index.blade.php

--- resources/views/subFolder/index.blade.php
+++ resources/views/subFolder/index.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Sub Folder') }}
+        </h2>
+    </x-slot>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         {{-- Conditionally Add Search Form if Parent Folder is Passed --}}
         @if(request()->has('parentFolder'))
