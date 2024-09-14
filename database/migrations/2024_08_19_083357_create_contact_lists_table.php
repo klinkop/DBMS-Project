438dc70ff9549d24f71683cb31eb59331729575f
Asrplynd <87851596+Asrplynd@users.noreply.github.com>
2024-09-14 13:24:59 +0800

    Update 2024_08_19_083357_create_contact_lists_table.php
    - Add status, type, product, address and remarks
    - Adjust the sequence

--- database/migrations/2024_08_19_083357_create_contact_lists_table.php
+++ database/migrations/2024_08_19_083357_create_contact_lists_table.php
@@ -16,15 +16,19 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('sub_folder_id')->constrained()->cascadeOnDelete();
             $table->string('name')->nullable(); // Allow null values
-            $table->string('status')->nullable(); // Allow null values
+            $table->unsignedBigInteger('status_id')->nullable(); // Allow null values
+            $table->unsignedBigInteger('type_id')->nullable();
+            $table->string('industry')->nullable(); // Allow null values
             $table->string('company')->nullable(); // Allow null values
+            $table->string('product')->nullable();
             $table->string('pic')->nullable(); // Allow null values
             $table->string('email')->nullable(); // Allow null values
             $table->string('contact1')->nullable(); // Allow null values
             $table->string('contact2')->nullable(); // Allow null values
-            $table->string('industry')->nullable(); // Allow null values
+            $table->string('address')->nullable(); // Allow null values
             $table->unsignedBigInteger('city_id')->nullable(); // Allow null values
             $table->unsignedBigInteger('state_id')->nullable(); // Allow null values
+            $table->string('remarks')->nullable();
             $table->timestamps();
         });
     }
