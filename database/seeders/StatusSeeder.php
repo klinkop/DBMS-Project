00054ffdc3e4d9c47e492eadcde60ecb60528ecf
Asrplynd <87851596+Asrplynd@users.noreply.github.com>
2024-09-14 13:45:22 +0800

    Add seeder for Status and Type

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Status;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $statuses = [
+            ['name' => 'Raw'],
+            ['name' => 'Ongoing'],
+            ['name' => 'Potential'],
+            ['name' => 'Existing'],
+            ['name' => 'KL-TG Raw'],
+            ['name' => 'KL-TG Ongoing'],
+            ['name' => 'KL-TG Potential'],
+            ['name' => 'KL-TG Existing'],
+        ];
+
+        foreach ($statuses as $status) {
+            Status::create($status);
+        }
+    }
+}
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = [
+            ['name' => 'A1'],
+            ['name' => 'A2'],
+            ['name' => 'Ongoing'],
+        ];
+
+        foreach ($types as $type) {
+            Type::create($type);
+        }
+    }
+}
